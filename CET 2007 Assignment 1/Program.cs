af3ceef8aba0de564de4fa7ab47557cd509819e1
Lauren Emmerson <bi76os@student.sunderland.ac.uk>
2025-10-28 20:59:27 +0000

    Updated player and game stats system. Added functionality to track hours played, high scores, and manage a basic game library through console input.
    fixed recordstats(). errors remain with interfaces

--- CET 2007 Assignment 1/Program.cs
+++ CET 2007 Assignment 1/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 namespace CE2007A1
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             PlayerManager playerManager = new PlayerManager();
+            GameLibrary gameLibrary = new GameLibrary();
             while (true)
             {
                 Console.WriteLine("Hi there!");
@@ -28,32 +30,67 @@ namespace CE2007A1
                 }
                 else if (choice == "2")
                 {
-                    //updating logic
+                    Console.WriteLine("Are you searching with Username or ID?");
+                    string Search = Console.ReadLine();
+                    Player FoundPlayer = null; //sets it to be empty to start
+                    if (Search == "Username" || Search == "username") //if looking via username...
+                    {
+                        Console.WriteLine("Please enter the username you're looking for:");
+                        string NameSearch = Console.ReadLine();
+                        FoundPlayer = playerManager.FindPlayerByUsername(NameSearch);
+                    }
+                    else if (Search == "ID" || Search == "id")  //if looking via ID...
+                    {
+                        Console.WriteLine("Please enter the player ID:");
+                        string IDSearch = Console.ReadLine();
+                        if (int.TryParse(IDSearch, out int id))
+                        {
+                            FoundPlayer = playerManager.FindPlayerByID(id);
+                        }
+                        else
+                        {
+                            Console.WriteLine("That ID is not recognised. Please try again.");
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid search type. Please type 'Username' or 'ID'.");
+                        continue; 
+                    }
+
+                    // if found player is recognised..
+                    if (FoundPlayer != null)  //found player is not null
+                    {
+                        Console.WriteLine($"Player found: {FoundPlayer.Username} ID: {FoundPlayer.ID}");
+                        Console.WriteLine("Displaying player stats now . . .");
+                        FoundPlayer.PlayerStats.RecordStats(gameLibrary); //calls record stats
+                    }
+                    else
+                    {
+                        Console.WriteLine("This player doesn't exist.");
+                    }
                 }
+
                 else if (choice == "3")
                 {
                     Console.WriteLine("What players stats do you want to see?");
                     //add viewing logic
                 }
-
-
             }
-            
         }
-
-
 
         class Player
         {
             public int ID { get; set; }
             public string Username { get; set; }
-            public Stats PlayerStats { get; set; } //links to stats block
+            public IUpdatableStats PlayerStats { get; set; } //links to stats block
 
             public Player(int id, string username)
             {
                 this.ID = id;
                 this.Username = username;
-                PlayerStats = new Stats(0, 0);  //blank values when players are made
+                PlayerStats = new Stats();
             }
         }
 
@@ -79,84 +116,120 @@ namespace CE2007A1
                 {
                     Console.WriteLine("You need to add a Username and an ID");
                 }
-                else if (!id.All(char.IsDigit))  //checks that its a number. info found at https://www.geeksforgeeks.org/c-sharp/c-sharp-char-isdigit-method/
+                else if (!id.All(char.IsDigit))  //checks that its a number
                 {
                     Console.WriteLine("The ID must contain numbers only");
                 }
                 else
                 {
-                    int ID = int.Parse(id); //converts string ID to int ID for easier handling
+                    int ID = int.Parse(id);
                     Console.WriteLine($"Added player {Username} with ID {ID} to list of players");
-                    players.Add(new Player(ID, Username)); // user inputted player is added to the list
-                    
-                    
+                    list.Add(new Player(ID, Username));
                 }
-
-
-
             }
-         
-        }
 
+            
+            public Player FindPlayerByID(int id)
+            {
+                return list.FirstOrDefault(p => p.ID == id);
+            }
+
+            public Player FindPlayerByUsername(string username)
+            {
+                return list.FirstOrDefault(p => p.Username == username);
+            }
+        }
 
         class Stats : IUpdatableStats
         {
-            public int HoursPlayed { get; private set; }
-            public int HighScore { get; private set; }
+            private List<GameStats> GameStatsList = new List<GameStats>();
 
-            public Stats(int hoursPlayed, int highScore)
+            public void RecordStats(GameLibrary gameLibrary) //enables use of Game and GameGenre
             {
-                this.HoursPlayed = hoursPlayed;
-                this.HighScore = highScore;
-            }
+                Console.WriteLine("Which game are you updating stats for? (please enter a game name)");
+                string gameNameInput = Console.ReadLine();
 
-            public void RecordStats()
-            {
-                int Hours;
-                
-                    Console.WriteLine("How many hours have you played on GAME?");
-                    string HoursPlayed= Console.ReadLine();
-                if (int.TryParse(HoursPlayed, out Hours) && Hours >= 0)
+                Game selectedGame = gameLibrary.FindGameByName(gameNameInput);
+                if (selectedGame == null)
                 {
-                    UpdateHoursPlayed(Hours);
-                }
-                else 
-                {
-                    Console.WriteLine("Sorry, this isnt a valid input. Please enter a postive number");
+                    Console.WriteLine("That game isn't in here! Add it instead!");
+                    return;
                 }
 
+                GameStats gameStats = GameStatsList.FirstOrDefault(gs => gs.Game == selectedGame);
+                if (gameStats == null)
+                {
+                    gameStats = new GameStats(selectedGame);
+                    GameStatsList.Add(gameStats);
+                }
 
+                Console.WriteLine($"How many hours have you played on {selectedGame.GameName}?");
+                string HoursPlayedInput = Console.ReadLine();
+                if (int.TryParse(HoursPlayedInput, out int hours) && hours >= 0)
+                {
+                    gameStats.HoursPlayed += hours;
+                    Console.WriteLine($"Updated hours played for {selectedGame.GameName}: {gameStats.HoursPlayed}");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you need to add a positive number");
+                }
 
-
-                    Console.WriteLine("Have you achieved a high score this time in GAME? /n Please answer Yes or No");
-                    string isHighScore = Console.ReadLine();
-                    if (isHighScore == "yes" || isHighScore == "Yes" || isHighScore == "y" || isHighScore == "Y")
+                Console.WriteLine($"Have you achieved a high score this time in {selectedGame.GameName}? \nPlease answer Yes or No");
+                string isHighScore = Console.ReadLine();
+                if (isHighScore == "yes" || isHighScore == "Yes" || isHighScore == "y" || isHighScore == "Y")
                     {
-                        Console.WriteLine("New high score logged into the system");
+                    Console.WriteLine($"What is your new high score in {selectedGame.GameName}?");
+                    string NewScoreInput = Console.ReadLine();
+                    if (int.TryParse(NewScoreInput, out int newScore))
+                    {
+                        if (newScore > gameStats.HighScore)
+                        {
+                            gameStats.HighScore = newScore;
+                            Console.WriteLine($"New high score logged into the system: {selectedGame.GameName}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Your score wasn't higher than your existing one! Current high score to beat: {gameStats.HighScore}.");
+                        }
                     }
                     else
                     {
-                    Console.WriteLine("Aww, thats a shame, better luck next time!");
+                        Console.WriteLine("That’s not a valid number!");
                     }
                 }
-                
-                
-            }
-
-
-            public void UpdateHighScore(int NewScore)
-            {
-                if (NewScore > HighScore)
-                    HighScore = NewScore;
-            }
-
-
-            public void UpdateHoursPlayed(int NewHours)
-            {
-                if (NewHours > 0)
+                else
                 {
-                    HoursPlayed += NewHours;
+                    Console.WriteLine("Aww, that's a shame, better luck next time!");
                 }
+            }
+
+            private class GameStats
+            {
+                public Game Game { get; private set; }
+                public int HoursPlayed { get; set; }
+                public int HighScore { get; set; }
+
+                public GameStats(Game game)
+                {
+                    Game = game;
+                    HoursPlayed = 0;
+                    HighScore = 0;
+                }
+            }
+
+            public void UpdateHighScore(Game selectedGame, int newScore) 
+            {
+                var gameStats = GameStatsList.FirstOrDefault(gs => gs.Game == selectedGame);  //finds existing game stats object for the game instead of making a new one every time 
+                if (gameStats != null && newScore>gameStats.HighScore)
+                    gameStats.HighScore = newScore;
+            }
+
+            public void UpdateHoursPlayed(Game selectedGame, int newHours)
+            {
+                var gameStats = GameStatsList.FirstOrDefault(gs => gs.Game == selectedGame);
+                if (gameStats != null && newHours > 0)
+                    gameStats.HoursPlayed += newHours;
             }
         }
 
@@ -164,48 +237,79 @@ namespace CE2007A1
         {
             void UpdateHighScore(int newscore);
             void UpdateHoursPlayed(int newhours);
+            void RecordStats(GameLibrary gameLibrary);
         }
 
-        class Game   //considering theres so many games in the game liubary it should be an abstract class for things like the name and id possibly genre
+        class Game
         {
+            private static Random ranID = new Random();
             public int GameID { get; set; }
             public string GameName { get; set; }
             public string GameGenre { get; set; }
 
-            public Game(int  gameID, string gameName, string GameGenre)
+            public Game(string gameName, string GameGenre)
             {
-                this.GameID = gameID;
+                this.GameID = ranID.Next(0, 99);
                 this.GameName = gameName;
                 this.GameGenre = GameGenre;
+            }
 
+            public override string ToString()
+            {
+                return $"{GameName} (ID: {GameID}) Genre: {GameGenre}";
             }
         }
+
         class GameLibrary
         {
             List<Game> games = new List<Game>();
 
-            public void AddGame(Game game) 
+            public void AddGame(Game game = null)
             {
-                // new game in game libary
-            }
-            public Game FindGameByID(int id)
-            {
-                //search logic
-            }
-            public void RemoveGame(Game game)
-            {
-                games.Remove(game); //maybe??
+                if (game != null)
+                {
+                    games.Add(game);
+                    Console.WriteLine($"Added game {game.GameName} - {game.GameGenre} to library!");
+                    return;
+                }
+
+                Console.WriteLine("Please add the name of the game you wish to add to your library");
+                string GName = Console.ReadLine();
+
+                Console.WriteLine($"Please enter the genre of {GName}");
+                string GGenre = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(GName) || string.IsNullOrWhiteSpace(GGenre))
+                {
+                    Console.WriteLine("Please enter a name and a genre of your game in the areas provided");
+                    return;
+                }
+                Game NewGame = new Game(GName, GGenre);
+                games.Add(NewGame);
+                Console.WriteLine("You have added a new game to your game library!");
             }
 
-            class Leaderboards
+            public Game FindGameByID(int GID)
             {
-                public List<Player> TopPlayersFromScore(List<Player> players) 
-                {
-                    //sort player by high score
-                }
-                public List<Player> TopActivePlayers(List<Player> players)
-                {
-                    //sort player by top activity
-                }
+                return games.FirstOrDefault(g => g.GameID == GID);
             }
+
+            public Game FindGameByName(string GName)
+            {
+                return games.FirstOrDefault(g => g.GameName == GName);
+            }
+        }
+
+        class Leaderboards
+        {
+            public List<Player> TopPlayersFromScore(List<Player> players)
+            {
+                return players;
+            }
+
+            public List<Player> TopActivePlayers(List<Player> players)
+            {
+                return players;
+            }
+        }
+    }
 }
