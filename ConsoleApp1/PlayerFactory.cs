af2d96f1c814ff611fbfb7e9083a761a58fae47e
lauren em <bi76os@student.sunderland.ac.uk>
2025-11-23 20:19:14 +0000

    Added back all the classes I missed into the new assignment. need to fix the errors

---
+++ ConsoleApp1/PlayerFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CET2007A1
+{
+    /// <summary>
+    /// Creates a reusable factory class for player creation. 
+    /// </summary>
+    public static class PlayerFactory
+    {
+        public static Player CreatePlayer(string username, int id)
+        {
+            return new Player(id, username);
+        }
+    }
+}
